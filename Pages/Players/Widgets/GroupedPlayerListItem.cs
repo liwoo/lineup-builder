0fa74058685d5de52fa5d3797ee5b4eabace395c
Daniel P. Mgawi <pdmgawi@gmail.com>
2024-01-05 19:49:57 +0200

    added grouped CollectionView Component

---
+++ Pages/Players/Widgets/GroupedPlayerListItem.cs
@@ -0,0 +1,25 @@
+using LineUp.Models;
+using MauiReactor;
+using System.Collections.Generic;
+
+namespace LineUp.Pages.Players.Widgets;
+
+public class GroupedListItem(Position position, IEnumerable<Player> Players) : Component
+{
+    public override VisualNode Render()
+    {
+        return new VStack()
+        {
+             new Label(position.ToString())
+             .FontSize(25)
+             .FontAttributes(MauiControls.FontAttributes.Bold)
+             .VerticalOptions(MauiControls.LayoutOptions.Center),
+            new CollectionView().ItemsSource(Players, PlayerListItem)
+        };
+    }
+
+    private static VisualNode PlayerListItem(Player player)
+    {
+        return new PlayerListItem(player);
+    }
+}
